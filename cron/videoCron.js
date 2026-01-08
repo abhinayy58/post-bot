334576b452e6a7df8f369c7c82e4fe95cdd1a3ce
Abhinay yadav <abhinayy48@gmail.com>
2026-01-08 14:56:00 +0530

    refactor: improve hashtag selection logic in video email cron job

--- cron/videoCron.js
+++ cron/videoCron.js
@@ -3,13 +3,18 @@ import Video from "../models/Video.js";
 // import { downloadFromCloudinary, getFileSizeMB } from "../utils/fileUtils.js";
 import { sendVideoEmail } from "../services/email.service.js";
 
+const getRandomHashtags = (hashtags, count = 5) => {
+  if (!Array.isArray(hashtags)) return [];
+  return [...hashtags].sort(() => Math.random() - 0.5).slice(0, count);
+};
+
 cron.schedule("0 19 * * *", async () => {
   console.log("⏰ Cron running");
 
   const video = await Video.findOne({ isUsed: false }).sort({ createdAt: -1 });
   if (!video) return;
 
-//   const filename = `${video.channel}_${video.msg_id}.mp4`;
+  //   const filename = `${video.channel}_${video.msg_id}.mp4`;
 
   try {
     // const localPath = await downloadFromCloudinary(
@@ -18,15 +23,17 @@ cron.schedule("0 19 * * *", async () => {
     // );
 
     // const sizeMB = getFileSizeMB(localPath);
- 
 
-      await sendVideoEmail({
-  caption: video.caption,
-  hashtags: video.hashtag,
-  videoLink: video.videourl
-});
+    const selectedHashtags =
+      video.hashtag.length > 5
+        ? getRandomHashtags(video.hashtag, 5)
+        : video.hashtag;
 
-
+    await sendVideoEmail({
+      caption: video.caption,
+      hashtags: selectedHashtags,
+      videoLink: video.videourl,
+    });
 
     video.isUsed = true;
     video.usedAt = new Date();
